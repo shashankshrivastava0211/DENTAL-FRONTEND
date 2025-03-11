1ba7d66012a1a98c30ad630c6b1938301421aa12
shashank shrivastava <internstudent02@gmail.com>
2025-03-12 00:55:32 +0530

    constants_removed

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,5 +1,1 @@
-// export const VITE_REACT_APP_BASE_URL = "/api/v1";
-
-export const VITE_REACT_APP_BASE_URL = "http://localhost:3000/api/v1";
-
-// export const VITE_REACT_APP_BASE_URL = "http://51.21.128.156/api/v1";
+export const VITE_REACT_APP_BASE_URL = "/api/v1";
